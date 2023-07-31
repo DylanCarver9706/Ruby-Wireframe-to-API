01b3a49cfe47abaab4beaf58d3879ea637009cf7
Dylan Carver <dylancarver14@gmail.com>
2023-07-30 23:13:33 -0500

    updated logo route path

--- wireframe-to-api-converter/src/Navbar.js
+++ wireframe-to-api-converter/src/Navbar.js
@@ -45,7 +45,7 @@ function Navbar() {
             variant="h6"
             noWrap
             component={Link}
-            to="/"
+            to="Ruby-Wireframe-to-API/"
             sx={{
               mr: 2,
               display: { xs: "none", md: "flex" },
