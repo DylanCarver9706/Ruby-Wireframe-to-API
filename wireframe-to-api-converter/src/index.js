ae0425625ee633a587942514353ba80e1a0ea04b
Dylan Carver <dylancarver14@gmail.com>
2023-07-31 10:33:52 -0500

    change deployment settings for vercel

--- wireframe-to-api-converter/src/index.js
+++ wireframe-to-api-converter/src/index.js
@@ -1,14 +1,13 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
